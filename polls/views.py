94c95fa1ff4b32c0290e8633902b615b8580cfa9
Peanutt47 <pnchalee@gmail.com>
2023-09-03 20:16:31 +0700

    fix Implement tutorial part 5

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,13 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from .models import Question, Choice
+from django.template import loader
+from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
-from .models import Choice, Question
+# Create your views here.
 
 
 class IndexView(generic.ListView):
@@ -22,7 +25,9 @@ class IndexView(generic.ListView):
 
 
 class DetailView(generic.DetailView):
-    ...
+    model = Question
+    template_name = 'polls/detail.html'
+
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
@@ -35,23 +40,23 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 
-def detail(request, question_id):
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
-def results(request, question_id):
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
 
-def vote(request, question_id):
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
