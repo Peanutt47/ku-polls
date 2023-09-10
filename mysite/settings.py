a7af3b53395623e048014cc593040baaed74cab1
Peanutt47 <pnchalee@gmail.com>
2023-09-10 19:42:40 +0700

    RedirectView

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default='django-insecure-eu_k2$i@p70@pi$koy#nnnyg9$e&#+ue_vnt+7=%qe@g*@n21')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool, default=True)
 
